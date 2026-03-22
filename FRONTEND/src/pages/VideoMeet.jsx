63c2a85da84c1dc8a02a4cc9eb72dd574c41a475
pritish <pritishkumar28@gmail.com>
2026-03-22 18:17:53 +0530

    video feature basic setup

--- FRONTEND/src/pages/VideoMeet.jsx
+++ FRONTEND/src/pages/VideoMeet.jsx
@@ -1,19 +1,107 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
+import {
+    TextField,
+    Button,
+    Container,
+    Typography,
+    Box,
+    Link,
+    CssBaseline,
+    Avatar,
+    Grid,
+} from '@mui/material';
 
 const url = "http://localhost:8000"
 
 var connections = {};
 
 const peerConnections = {
-    "iceServer" : [
-        {"urls" : "stun:stun.l.google.com:19302"}
+    "iceServer": [
+        { "urls": "stun:stun.l.google.com:19302" }
     ]
 }
 
-export default function VideoMeetComponent (){
-    return 
-    <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, repellendus. Amet incidunt voluptatum beatae minus similique recusandae fuga quod magni vel. Cupiditate sed nemo numquam adipisci id quo omnis alias
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate ipsum nisi ullam, unde veritatis a excepturi explicabo quae quidem perferendis delectus ex cupiditate eligendi sint. Quis nostrum sint vitae iste perferendis illum ducimus recusandae temporibus assumenda libero. Ea sapiente esse ab voluptates dolores ratione adipisci minima similique obcaecati omnis iusto, saepe aliquam iure eum quia repellendus. Corrupti atque omnis aspernatur neque quo eligendi expedita reiciendis excepturi animi ab sapiente voluptatem eveniet non veniam, nobis laudantium sequi! Molestiae nesciunt suscipit necessitatibus consequuntur atque perspiciatis voluptates ducimus culpa eius, corrupti exercitationem, delectus, quas reiciendis magnam. Voluptate et similique deserunt atque fugit consectetur magni optio inventore ut placeat blanditiis, neque repellat fuga beatae, nesciunt nulla voluptas aut repellendus. Illum nemo vitae fugiat laborum amet magnam delectus excepturi quo placeat repudiandae voluptate aut eligendi, dolorum totam et, quia ipsa nisi laboriosam neque temporibus exercitationem cum animi! Maiores voluptas nostrum nam eum laudantium. Molestiae, amet aliquid cumque quidem consequatur atque magni soluta, deserunt voluptas itaque cupiditate consectetur ducimus repudiandae excepturi eum id nihil et quia deleniti rem saepe, porro non. Repudiandae laborum maiores odio deserunt, deleniti eligendi iusto nobis assumenda alias facere nulla voluptatibus quod. Aspernatur quas similique fugit atque cupiditate placeat nemo rem aut?
-    </div>
+export default function VideoMeetComponent() {
+
+    var socketRef = useRef();
+    let socketIdRef = useRef();
+
+    let localVideoRef = useRef();
+
+    let [videoAvailable, setVideoAvailable] = useState(true);
+
+    let [audioAvailable, setAudioAvailable] = useState(true);
+
+    let [video, setVideo] = useState();
+
+    let [audio, setAudio] = useState();
+
+    let [screen, setScreen] = useState();
+
+    let [showModel, setModel] = useState();
+
+    let [screenAvailable, setScreenAvailable] = useState();
+
+    let [messages, setMessages] = useState([]);
+
+    let [message, setMessage] = useState("")
+
+    let [newMessages, serNewMessages] = useState(0);
+
+    let [askForUsername, setaAskForUsername] = useState(true);
+
+    let [username, setUsername] = useState("");
+
+    const videoRef = useRef();
+
+    let [videos, setVideos] = useState();
+
+    // TODO
+
+    // if(isChrome() === false) {
+
+
+    // } 
+
+
+    const getPermission = async () => {
+        try {
+            const videoPermission = await navigator.mediaDevices.getUserMedia({ video: true });
+            if (videoPermission) {
+                setVideoAvailable(true);
+            } else {
+                setVideoAvailable(false)
+            }
+            const audioPermission = await navigator.mediaDevices.getUserMedia({ audio: true });
+            if (audioPermission) {
+                setAudioAvailable(true);
+            } else {
+                setAudioAvailable(false)
+            }
+        } catch {
+
+        }
+    }
+    useEffect(() => {
+        getPermission();
+    }, []);
+
+    return (
+        <div>
+            {
+                askForUsername === true ?
+                    <div>
+                        <h3> Enter to Lobby</h3>
+                        <TextField id="outlined-basic" label="username" variant="outlined" value={username} onChange={e => setUsername(e.target.value)} />
+                        <Button variant="contained">connect</Button>
+                        <div>
+                            <video ref={localVideoRef} autoPlay muted></video>
+                        </div>
+                    </div> : <>
+
+
+                    </>
+            }
+        </div>
+    )
 }
